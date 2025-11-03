9b8282f722b7e5ea782ce5b57a1f9c350d60c5e2
tmitsuya <tmitsuya@student.codam.nl>
2025-11-03 18:35:58 +0100

    modified mariadb dockerfile to delete binding to the 127.0.0.1 address and entrypoint.sh that create database and users by running deamon server temporarily and client to command

--- srcs/requirements/mariadb/tools/docker-entrypoint.sh
+++ srcs/requirements/mariadb/tools/docker-entrypoint.sh
@@ -1,60 +1,121 @@
-#!bin/bash
+#!/bin/bash
 
-user=mysql
-datadir=/var/lib/mysql
-database_already_exist='false'
+set -eu
 
-function basic_single_escape ()
+function docker_setup_query()
 {
-    # The quoting on this sed command is a bit complex.  Single-quoted strings
-    # don't allow *any* escape mechanism, so they cannot contain a single
-    # quote.  The string sed gets (as argv[1]) is:  s/\(['\]\)/\\\1/g
-    #
-    # Inside a character class, \ and ' are not special, so the ['\] character
-    # class is balanced and contains two characters.
-    echo "$1" | sed 's/\(['"'"'\]\)/\\\1/g'
+	# Read from the environment variables or the secrets files for password
+	user="$MARIADB_USER"
+	database="$MARIADB_DATABASE"
+	root_password="$(< "${MARIADB_ROOT_PASSWORD_FILE}")"
+	password="$(< "$MARIADB_PASSWORD_FILE")"	
+	
+	# Securing system user
+	# https://mariadb.com/docs/server/clients-and-utilities/deployment-tools/mariadb-secure-installation
+	# (test database is skipped during the installation)
+	set_root_localhost_pass="SET PASSWORD FOR 'root'@'localhost'= PASSWORD( '${root_password}' );"
+	remove_remote_root="DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
+	remove_anonymous_users="DELETE FROM mysql.global_priv WHERE User='';"
+
+	# Create mysql@localhost
+	create_mysql="CREATE USER mysql@localhost IDENTIFIED VIA unix_socket;"
+	mysql_grants="GRANT ALL PRIVILEGES ON *.* TO mysql@localhost;"
+
+	# Create the database and the user by using the mariadb client
+	# https://developer.wordpress.org/advanced-administration/before-install/creating-database/
+	create_data_base="CREATE DATABASE IF NOT EXISTS $database;"
+	create_user="CREATE USER '$user'@'%' IDENTIFIED BY '$password';"
+	user_grants="GRANT ALL PRIVILEGES ON ${database}.* TO '$user'@'%';"	
+
+	# Reloading the privilege tables will ensure that all changes made
+	reload_privilege_tables="FLUSH PRIVILEGES;"
 }
 
-function docker_setup_env()
+function docker_install_db()
 {
-    mariadb_root_password="$(basic_single_escape "$(cat "$MARIADB_ROOT_PASSWORD_FILE")")"
-    mariadb_password="$(basig_single_escape "$(cat "$MARIADB_PASSWORD_FILE")")"
-    if [ -d "$datadir/mysql" ]; then
-        database_already_exist='true'
-    fi
+	# echo "Create directories for the database, the socket and lock files"
+	# mkdir -p "$datadir" "/run/mysqld"
+	# chown -R "mysql:mysql" "$datadir" "/run/mysqld"
+	# ensure that /run/mysqld (used for socket and lock files) is writable 
+	# regardless of the UID our mysqld instance ends up having at runtime
+	# chmod 1777 "/run/mysqld"
+	
+	echo "Start the mariadb install process"
+	mariadb-install-db --datadir=$MARIADB_VOLUME --skip-test-db
+	echo "End of the install process"
 
+	chown -R "mysql:mysql" $MARIADB_VOLUME
 }
 
-function docker_exec_client() {
-	# args sent in can override this db, since they will be later in the command
-	if [ -n "$MYSQL_DATABASE" ]; then
-		set -- --database="$MYSQL_DATABASE" "$@"
+function docker_setup_db()
+{
+	# temporarily run the mariadb server in the background
+	echo "Starting temporary mariadb server in the background"
+	mariadbd --user=mysql --datadir="$MARIADB_VOLUME" &
+
+	# PID of the last background command, e.g. mariadb daemon
+	declare -g MARIADB_PID
+	MARIADB_PID=$!
+
+	echo "Waiting for server startup"
+	local i
+	for i in {30..0}; do
+		if mariadb -uroot -hlocalhost --protocol=socket --database=mysql \
+			-e "SELECT 1" > /dev/null 2>&1; then
+			break
+		fi
+		sleep 1
+	done
+	if [ "$i" = 0 ]; then
+		echo "Unable to start server."
+		exit 1
 	fi
-	mariadb --protocol=socket -uroot -hlocalhost --socket="${SOCKET}" "$@"
+
+	# tell docker_process_sql to not use MARIADB_ROOT_PASSWORD since it is just now being set
+	mariadb -uroot -hlocalhost --protocol=socket --database=mysql <<-EOSQL
+		-- setting root password and system user (equivalent to running mysql_secure_installation)
+		${set_root_localhost_pass}
+		${remove_remote_root}
+		${remove_anonymous_users}
+		${create_mysql}
+		${mysql_grants}
+		-- end of setting system users, rest of init now...
+
+		-- create users/databases
+		${create_data_base}
+		${create_user}
+		${user_grants}
+
+		${reload_privilege_tables}
+	EOSQL
+
+	# stop the server and wait it
+	echo "Stopping temporary mariadb server"
+	kill "$MARIADB_PID"
+	wait "$MARIADB_PID"
 }
 
 
-function docker_cleanup_env()
-{
-    unset 'MARIADB_ROOT_PASSWORD'
-    unset 'MARIADB_PASSWORD'
-}
+# define database directory if it's not been set
+if [ -z $MARIADB_VOLUME ]; then
+	export "MARIADB_VOLUME=/var/lib/mysql"
+	mkdir -p $MARIADB_VOLUME
+fi
 
-function docker_secure_install()
-{
-    echo "Securing system users (equivalent to running mysql_secure_installation)"
-    # set a password for root accounts.
-    root_localhost_pass="SET PASSWORD FOR 'root'@'localhost'= PASSWORD('${mariadb_root_password}');"
-    remove_remote_root="DELETE FROM mysql.global_priv WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');"
-    remove_anonymous_users="DELETE FROM mysql.global_priv WHERE User='';"
-    remove_test_database="DROP DATABASE IF EXISTS test;"
-    remove_privileges_on_test_database="DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%'"
+# Install a database if it's not been set up yet
+if [ ! -d "${MARIADB_VOLUME}/mysql" ]; then
+	echo "initialize MariaDB"
+	docker_setup_query
+	docker_install_db
+	docker_setup_db
+	echo "Ready to run the server"
+else
+	# Even if the database already exists, still need to change the ownership of the newly created directory used for the database
+	chown -R "mysql:mysql" $MARIADB_VOLUME
+fi
 
-
-}
-
-docker_setup_env
-if [ $database_already_exist != 'true']
-    mariadb-install-db --datadir=$datadir --
-    docker_secure_install
-exec "$@"
+if [ "$1" = "mariadbd" ]; then
+	exec "$@" --user=mysql --datadir="$MARIADB_VOLUME"
+else
+	exec "$@"
+fi

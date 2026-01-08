80987a46b6978753fe454440a806cfb7b18c20a6
tmitsuya <tmitsuya@student.codam.nl>
2026-01-08 16:24:08 +0100

    care for the ownership of ftp user's home directory

--- srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
+++ srcs/requirements/bonus/ftp/tools/docker-entrypoint.sh
@@ -7,6 +7,11 @@ if [ -z ${FTP_VOLUME:-} ]; then
 	mkdir -p $FTP_VOLUME
 fi
 
+# For anonymous FTP, need the user to have a valid home directory (which is NOT owned or writable by the user "ftp").
 usermod -d $FTP_VOLUME ftp
+chown root.root $FTP_VOLUME
+
+# Remove write permission from group and others
+chmod og-w $FTP_VOLUME
 
 exec "$@"
